832fd71087accc15f1138b36889b7ec63cc67af0
bharathi kannan <bharathikannan1311@gmail.com>
2021-02-04 13:07:07 +0530

    website update

--- Data Entering/Specific Details/includesspecificdata/dbhspecificdata.inc.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-$dbServername="localhost";
-$dbUsername="root";
-$dbPassword="";
-$dbName="dataentering";
-
-$conn = mysqli_connect($dbServername,$dbUsername,$dbPassword,$dbName);
--- footer.php
+++
@@ -1,3 +0,0 @@
-<footer>
-	<p>Hello</p>
-</footer>
